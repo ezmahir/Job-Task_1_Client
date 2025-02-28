f916e5963a00213931a3f6714592a81b2029f274
ProgrammingHero1 <mahirruphy@gmail.com>
2025-02-28 15:09:23 +0600

    Final Push

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ function App() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const apiUrl = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+  const apiUrl =
+    import.meta.env.VITE_API_URL || "https://job-task-server-smoky.vercel.app";
 
   useEffect(() => {
     fetchTasks();
